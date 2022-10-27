e4ea9266fcfd386c34c8b0c42fa6c044cee91afe
ql-njupt <2938586096@qq.com>
2022-10-27 14:37:12 +0800

    接口2

--- sql/sql_method.h
+++ sql/sql_method.h
@@ -18,8 +18,8 @@ public:
     //查找对应cookie是否在数据库中,如果在数据库中，则返回true并将用户名写入id变量中；如果不在，返回false，id变量不管
     bool find_cookie(const string& cookie, string& id);
     //插入新用户，如果成功返回true，失败返回false
-    bool new_user(const string& user_id, const string& passwd);
+    bool new_user(const string& Uname, const string& passwd, const string& Age, const string& Sex);
     //确认用户，查找是否有这个用户并且密码正确如果没问题则返回true，有问题返回false
-    bool confirm_user(const stirng& user_id, const string& passwd);
-    void set_cookie(const string& cookie, const string& name);
+    bool confirm_user(const string& user_id, const string& passwd);
+    bool set_cookie(const string& cookie, const string& name);
 }
