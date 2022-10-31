63c4aea97a1cdb437cf30bf2fb01cac9f939f92c
ql-njupt <2938586096@qq.com>
2022-10-31 19:45:42 +0800

    10-31

--- sql/sql_method.cc
+++ sql/sql_method.cc
@@ -1,4 +1,41 @@
 #include "sql_method.h"
+
+bool M_sql::set_cookie(const string& cookie, const string& id)
+{
+	string temp = "";
+	if (find_cookie(cookie, temp))
+	{
+		string sql1 = "update cookie set Uid='" + id + "' where cok = '" + cookie + "' ";
+		bool ret = mysql_query(link, sql1.c_str());
+		cout << "执行Uid修改语句" << endl;
+		if (!ret)
+		{
+			cout << "修改Uid失败" << endl;
+			return false;
+		}
+		else
+		{
+			cout << "修改Uid成功"; << endl;
+			return true;
+		}
+	}
+	else
+	{
+		string sql = "insert into cookie(cok, Uid) values ('" + cookie + "','" + id + "')";
+		//cout << sql << endl;
+		int ret = mysql_query(link, sql.c_str());
+		cout << "执行cookie插入语句" << endl;
+		if (ret < 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
+
 bool M_sql::find_cookie(const string& cookie, string& id)
 {
 	string sql = "select Uid from cookie where cok = '"+cookie + "'";
@@ -29,10 +66,9 @@ bool M_sql::find_cookie(const string& cookie, string& id)
 	}
 }
 
-bool M_sql::new_user(const string& Uname, const string& passwd, const string& Age, const string& Sex)
+bool M_sql::new_user(const string& Uname, const string& passwd, const string& Age, const string& Sex, const string& is_company)//合并了个人和企业表，个人F，企业T
 {
-	    //string sql = "insert into person(uname, ped, age, sex) values("+Uname+","+ passwd+"," + Age + "," + Sex + ")";
-		string sql = "insert into user(uname, ped, age, sex) values ('" + Uname + "','" + passwd + "','" + Age +  "','" + Sex +"')";
+		string sql = "insert into user(uname, ped, age, sex, is_company) values ('" + Uname + "','" + passwd + "','" + Age +  "','" + Sex +"','"+is_company+"')";
 		int ret = mysql_query(link, sql.c_str());
         cout << "执行插入新用户语句" << endl;
 		if (ret < 0)
@@ -45,18 +81,18 @@ bool M_sql::new_user(const string& Uname, const string& passwd, const string& Ag
 		}
 }
 
-bool M_sql::confirm_user(const string& user_id, const string& passwd)
+int M_sql::confirm_user(const string& user_id, const string& passwd)//改变返回返回值，个人1，企业2，错0
 {
     cout << user_id << endl;
-    string sql = "select Uname, Ped from user where Uname = '"+user_id + "'";
+    string sql = "select Uname, Ped，is_company from user where Uname = '"+user_id + "'";
     cout << "登陆前" << endl;
     if(mysql_query(link, sql.c_str())){
         cout << "查询失败" << endl;
-        return false;
+        return 0;
     }
     if(!(result = mysql_store_result(link))){
         cout << "结果集获取失败";
-        return false;
+        return 0;
     }
     cout << "登陆后" << endl;
 	if (row = mysql_fetch_row(result))
@@ -64,23 +100,64 @@ bool M_sql::confirm_user(const string& user_id, const string& passwd)
         cout << "确认用户中" << endl;
 		if (row[0] == user_id && row[1] == passwd)
 		{
-			return true;
+			cout << "确认完成，区分个人与企业" << endl;
+			//当用户id及密码匹配后，区分个人和企业
+			if (row[2] == "T")
+			{
+				return 2;
+			}
+			else
+				return 1;
 		}
 		else
-			return false;
+			return 0;
 	}
 	else
 	{
+		return 0;
+	}
+}
+
+bool M_sql::find_user(const string& name, string& Ped ,string& Age, string& sex, string& is_company)//显示用户的所有信息
+{
+	string sql = "select Ped,Age,Sex,is_company from user where Uname = '" + name + "'";
+	cout << "查询用户信息前" << endl;
+	if (!mysql_query(link, sql.c_str())) {
+		cout << "查询成功" << endl;
+	}
+	else {
+		cout << "查询失败" << endl;
+		return false;
+	}
+	if ((result = mysql_store_result(link))) {
+		cout << "得到结果集" << endl;
+	}
+	else {
+		cout << "没有得到结果集" << endl;
+		return false;
+	}
+	cout << "查询用户信息后" << endl;
+	if (row = mysql_fetch_row(result))
+	{
+		cout << "成功" << endl;
+		Ped = row[0];
+		Age = row[1];
+		sex = row[2];
+		is_company = row[3];
+		return true;
+	}
+	else
+	{
+		cout << "失败" << endl;
 		return false;
 	}
 }
 
-bool M_sql::set_cookie(const string& cookie, const string& name)
+bool M_sql::new_post(const string& head, const string& author, const string& content)//插入帖子
 {
-	string sql = "insert into cookie(cok, Uid) values ('"+cookie+"','"+name+"')";
-    //cout << sql << endl;
+	string sql = "insert into post(title, author, content) values ('" + head + "','" + author + "','" + content + "')";
 	int ret = mysql_query(link, sql.c_str());
-    cout << "执行cookie插入语句" << endl;
+	cout << "执行插入新贴子语句" << endl;
 	if (ret < 0)
 	{
 		return false;
@@ -90,3 +167,41 @@ bool M_sql::set_cookie(const string& cookie, const string& name)
 		return true;
 	}
 }
+
+bool M_sql::show_post(string& ret)
+{
+	unsigned int num_fields;
+	string sql = "select Author, title, content from post";
+	cout << "显示帖子前" << endl;
+	if (!mysql_query(link, sql.c_str())) {
+		cout << "查询成功" << endl;
+	}
+	else {
+		cout << "查询失败" << endl;
+		return false;
+	}
+	if ((result = mysql_store_result(link))) {
+		cout << "得到结果集" << endl;
+	}
+	else {
+		cout << "没有得到结果集" << endl;
+		return false;
+	}
+	cout << "查询后" << endl;
+	num_fields = mysql_num_fields(result);
+	cout << "成功" << endl;
+	ret += "<!DOCTYPE html><html>\r\n";
+	ret += "<head><title>帖子</title><meta http-equiv=\"Content-Type\" content=\"textml;charset=utf-8\" /> </head>\r\n";
+	ret += "<body>";
+	while (row = mysql_fetch_row(result))
+	{
+		ret += "<p><big>";
+		ret += row[1] + "</big>";
+		ret += row[0] + "</p><br/>";
+		ret += "<p>" + row[2] + "</p><br/>";
+	}
+	return true;
+}
+
+
+
--- sql/sql_method.h
+++ sql/sql_method.h
@@ -17,11 +17,19 @@ public:
     }
     ~M_sql() = default;
 
+    bool set_cookie(const string& cookie, const string& id);
+
     //查找对应cookie是否在数据库中,如果在数据库中，则返回true并将用户名写入id变量中；如果不在，返回false，id变量不管
     bool find_cookie(const string& cookie, string& id);
+
     //插入新用户，如果成功返回true，失败返回false
-    bool new_user(const string& Uname, const string& passwd, const string& Age, const string& Sex);
-    //确认用户，查找是否有这个用户并且密码正确如果没问题则返回true，有问题返回false
-    bool confirm_user(const string& user_id, const string& passwd);
-    bool set_cookie(const string& cookie, const string& name);
+    bool new_user(const string& Uname, const string& passwd, const string& Age, const string& Sex, const string& is_company);
+
+    int confirm_user(const string& user_id, const string& passwd);
+    
+    bool find_user(const string& name, string& Ped ,string& Age, string& sex, string& is_company);//显示用户的所有信息
+
+    bool new_post(const string& head, const string& author, const string& content);//插入帖子
+
+    bool show_post(string& ret);//显示所有帖子
 };
